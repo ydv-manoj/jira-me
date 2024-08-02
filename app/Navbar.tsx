db4267cf2eb0911b0ce359af65880fe38b2dab98
Manoj Yadav <manojyadav@Manojs-MacBook-Air-2.local>
2024-08-02 13:13:55 +0530

    setup prisma and classnames

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,12 @@
+"use client"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { AiFillBug } from "react-icons/ai";
-
+import classnames from 'classnames';
 const Navbar = () => {
+
+    const currPath=usePathname()
     const links=[
         {label:'Dashboard',href:'/'},
         {label:'Issues',href:'/issues'}
@@ -11,7 +15,11 @@ const Navbar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><AiFillBug/></Link>
         <ul className='flex space-x-6'>
-            {links.map((link)=><Link className='text-zinc-500 hover:text-zinc-800 transition-colors' key={link.href} href={link.href}>{link.label}</Link>)}
+            {links.map((link)=><Link className={classnames({
+                'text-zinc-900':link.href===currPath,
+                'text-zinc-500':link.href!==currPath,
+                'hover:text-zinc-800 transition-colors':true
+            })} key={link.href} href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Navbar/>
-        <main>{children}</main>
+        <main className="px-4">{children}</main>
         </body>
     </html>
   );
