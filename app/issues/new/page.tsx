a79d017546a70c4bd12f1e22df7ce2feb0834fcd
ydv-manoj <mojirao770@gmil.com>
2024-08-03 13:54:01 +0530

    add a spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-// import SimpleMDE from "react-simplemde-editor";
+import { Button, Callout, Spinner, Text, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm,Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -9,9 +9,10 @@ import { useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 
-import dynamic from 'next/dynamic';
+
 import { createIssueSchema } from '@/app/validationSchemas';
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
+import ErrorMessage from '@/app/components/ErrorMessage';
+  
 
 
 type IssueForm =z.infer<typeof createIssueSchema>
@@ -22,6 +23,7 @@ const NewIssuePage = () => {
       resolver:zodResolver(createIssueSchema)
   });
   const [error,setError]=useState("")
+  const [submitting,setSubmitting]=useState(false)
 
 
   return (
@@ -35,22 +37,24 @@ const NewIssuePage = () => {
         onSubmit={
             handleSubmit(async (data)=>{
                 try {
+                    setSubmitting(true)
                     await axios.post('/api/issues',data);
                     router.push('/issues') 
                 } catch (error) {
+                    setSubmitting(false)
                     setError('An unexpected error occured')
                 }
             })
             }>
             <TextField.Root placeholder='Title' {...register('title')}></TextField.Root>
-            {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+            <ErrorMessage>{errors.title?.message}</ErrorMessage>
             <Controller 
                 name='description'
                 control={control}
                 render={({field})=><SimpleMDE placeholder='Description' {...field} />}
             />
-            {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
-            <Button>Submit New Issue</Button>
+            <ErrorMessage>{errors.description?.message}</ErrorMessage>
+            <Button disabled={submitting}>Submit New Issue {submitting && <Spinner/>}</Button>
         </form>
     </div>
   )
